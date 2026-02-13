7eef3f87a8fe24d6e5b3383c4a2e2db21162daca
Kishore-bot11 <kishoreravi1111@gmail.com>
2026-02-13 19:36:12 +0530

    Cursor effect

--- script.js
+++ script.js
@@ -151,7 +151,33 @@ document.addEventListener("DOMContentLoaded", () => {
     mobileMenu.classList.remove("active");
   });
 
+document.addEventListener("DOMContentLoaded", function () {
 
+  const cursor = document.getElementById("cursor-motion");
+
+  let mouseX = 0;
+  let mouseY = 0;
+  let posX = 0;
+  let posY = 0;
+
+  document.addEventListener("mousemove", function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  function animate() {
+    posX += (mouseX - posX) * 0.1;
+    posY += (mouseY - posY) * 0.1;
+
+    cursor.style.left = posX + "px";
+    cursor.style.top = posY + "px";
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+
+});
 
 
 
